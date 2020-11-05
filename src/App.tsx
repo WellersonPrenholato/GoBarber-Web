f8e196fd71a95ecd722980bde505ff90211a44f4
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2020-11-05 14:44:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const App: React.FC = () => (
-  <h1>Hello Word</h1>
-);
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <SignUp />
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
